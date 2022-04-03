1c5f3b4ec13294c8d6743b0af12307a537fb56fa
stho32 <stho32github@gmail.com>
2022-04-03 12:04:08 +0200

    the script to start the database is now repeatable

--- 01_start-database_in_separate_terminal.sh
+++ 01_start-database_in_separate_terminal.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 docker run -p 5432:5432 --env POSTGRES_PASSWORD=password --name employee-feedback postgres:13.3-alpine
 
-echo "Now please make sure that the .env-file is present"
+# cleanup afterwards
+echo ""
+echo ""
+echo ""
+echo "Removing database docker image again..."
+docker rm employee-feedback
+
